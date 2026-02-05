98fee2e751e0c3063e64d4585838ad5d7cdfcd05
Arsik <arstroyan@gmail.com>
2026-02-06 02:36:33 +0300

    Update GiphApi.kt

--- app/src/main/java/com/example/pictureapi/API/GiphApi.kt
+++ app/src/main/java/com/example/pictureapi/API/GiphApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 interface GiphApi {
     @GET("v1/gifs/search")
     suspend fun getPhotos(
-        @Query("api_key") apiKey: String = "kgxnfPUL36RHC426TgCXtE9ERHT1hi2x",
+        @Query("api_key") apiKey: String = API_KEY,
         @Query("q") q: String = "cat",
 
         ): GiphResponse
